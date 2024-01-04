9f9311aeff397a4734912f9b280ecab8d3b2a590
erenakgl <ernakgl00@gmail.com>
2024-01-04 13:16:15 +0300

    Bug fixed build table

--- CustomScripts/BuildTable.js
+++ CustomScripts/BuildTable.js
@@ -1,25 +1,8 @@
 ﻿$(document).ready(function () {
-    $('#todoCreateFrom').submit(function (e) {
-        e.preventDefault();
-
-        $.ajax({
-            url: '/ToDoes/AJAXCreate',
-            type: 'POST',
-            data: $(this).serialize(),
-            success: function (result) {
-                if (result.success) {
-                    // Redirect to the ToDoes page
-                    window.location.replace('/ToDoes/Index');
-                } else {
-                    // Handle the case where the ToDo creation fails
-                    console.error(result.message);
-                    // Optionally display an error message to the user
-                }
-            },
-            error: function (xhr, status, error) {
-                console.error("Error creating ToDo:", error);
-                // Optionally display an error message to the user
-            }
-        });
-    });
-});
+    $.ajax({
+        url: '/ToDoes/BuildToDoTable',
+        success: function (result) {
+            $('#tableDiv').html(result);
+        }
+    })
+})
